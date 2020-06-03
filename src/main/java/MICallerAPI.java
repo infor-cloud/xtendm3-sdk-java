56f103fbec762ec85c6db48d350e699e3d015798
Erfan Yousefi <er.yousefi@gmail.com>
2020-06-04 01:22:35 +0200

    Add support for date format and max records on MICaller

--- src/main/java/MICallerAPI.java
+++ src/main/java/MICallerAPI.java
@@ -29,4 +29,20 @@ public interface MICallerAPI {
    * @since API Version 0.1.0
    */
   void call(String program, String transaction, Map<String, String> parameters, Closure<?> callback);
+
+  /**
+   * Sets the max amount of responses to receive when calling a list transaction
+   *
+   * @param maxRecords Max records
+   * @since API Version 0.14.0
+   */
+  void setListMaxRecords(int maxRecords);
+
+  /**
+   * Sets the date format. Available formats: YMD8, YMD6, MDY6, DMY6, YWD5
+   *
+   * @param dateSep  Date separator
+   * @param dateForm Date format
+   */
+  void setDateFormat(char dateSep, String dateForm);
 }
