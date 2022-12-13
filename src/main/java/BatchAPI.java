59e1f2f9e8007cb2a1ce7282bb97c6306bc57770
fhakansson <filip.hakansson@infor.com>
2022-12-13 14:13:35 +0100

    Added Batch APIs to the SDK

---
+++ src/main/java/BatchAPI.java
@@ -0,0 +1,23 @@
+/**
+ * API for batch
+ *
+ * @since API Version 0.23
+ */
+public interface BatchAPI {
+
+  /**
+   * Getter for job id
+   *
+   * @return Selected job id
+   * @since API Version 0.23
+   */
+  String getJobId();
+
+  /**
+   * Getter for reference id
+   *
+   * @return Selected reference id
+   * @since API Version 0.23
+   */
+  String getReferenceId();
+}
